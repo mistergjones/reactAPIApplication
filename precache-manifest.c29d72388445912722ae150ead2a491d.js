5547022cd393f2c304f7b7fdee486de60b910b93
Glen <mistergjones@yahoo.com.au>
2020-06-25 22:58:54 +1000

    Updates

---
+++ precache-manifest.c29d72388445912722ae150ead2a491d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42214a354f260170766bb4b5a144d2d",
+    "url": "/reactAPIApplication/index.html"
+  },
+  {
+    "revision": "41cf3ee52c6b87f1bb7e",
+    "url": "/reactAPIApplication/static/css/main.1f2f31dc.chunk.css"
+  },
+  {
+    "revision": "cf7ef9d72dca2027d4d2",
+    "url": "/reactAPIApplication/static/js/2.ccd1f839.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactAPIApplication/static/js/2.ccd1f839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cf3ee52c6b87f1bb7e",
+    "url": "/reactAPIApplication/static/js/main.52ee3772.chunk.js"
+  },
+  {
+    "revision": "2e1576bfd88917e02b14",
+    "url": "/reactAPIApplication/static/js/runtime-main.79c84ec3.js"
+  }
+]);
